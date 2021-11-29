4a88417a40a1d3335c23f66b55597069211c0156
terjekand <42352088+terjekand@users.noreply.github.com>
2021-11-29 12:16:09 +0100

    Update Calculate.java

--- survey/src/main/java/com/dynata/service/Calculate.java
+++ survey/src/main/java/com/dynata/service/Calculate.java
@@ -6,18 +6,23 @@ import com.dynata.model.Survey;
 import java.util.List;
 
 public class Calculate {
+    
+    //sum of lines Participation.csv / participation list length
     public List<Member> calculateCompletedSurveys (){
         return null;
     }
-
+    
+    //sum of lines Participation.csv by memberId 
     public List<Survey> gcalculateCompletedSurveysByOneMember (){
         return null;
     }
-
+      
+    //sum of gained points Participation.csv by memberId and Surveys.csv by SurveyId
     public int calculateGainedPointsByOneMember () {
-        return 0;
+        return 0
     }
-
+    
+    //
     public List<Member> calculateNotAskedMembersForSurvey () {
         return null;
     }
